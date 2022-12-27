9af2add5eb2e898205e95225ccd6b4fe812b2de3
fernando flores <martinjanns2000@gmail.com>
2022-12-27 09:10:42 -0600

    se modifico el error de stripe

--- src/components/ButtonStripe.jsx
+++ src/components/ButtonStripe.jsx
@@ -1,22 +1,33 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import ecommerce from "../api/ecommerceApi";
+import { vaciarCart } from "../store/slices/CarritoSlices";
 
 export const ButtonStripe = () => {
-
-  const { cart } = useSelector(store => store.cart)
+  const { cart } = useSelector((store) => store.cart);
+  const dispatch = useDispatch()
 
   const sendProducts = async () => {
+    try {
+      const { data } = await ecommerce.post("/create-checkout-session", {
+        cart,
+      });
 
-    const { data } = await ecommerce.post('/create-checkout-session', { cart })
-    
-    if(data.URL) {
-      window.location = data.URL
+      if (data.URL) {
+        window.location.href = data.URL;
+      }
+
+      dispatch( vaciarCart() )
+
+    } catch (error) {
+      console.log(error);
     }
-    console.log(data)
-  }
+  };
 
   return (
-    <button onClick={sendProducts} className="block w-full py-2 px-4 text-center text-white bg-yellow-500 mt-2 rounded ">
+    <button
+      onClick={sendProducts}
+      className="block w-full py-2 px-4 text-center text-white bg-yellow-500 mt-2 rounded "
+    >
       check out
     </button>
   );
